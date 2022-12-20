2023297d4986cc135a7f92b9621b0b7e3aa587ff
MusketPosition <musket.gun@position.exchange>
2022-12-20 10:32:55 +0700

    add get batch pending limit orders

--- contracts/implement/SpotDex.sol
+++ contracts/implement/SpotDex.sol
@@ -371,6 +371,32 @@ abstract contract SpotDex is ISpotDex, SpotHouseStorage {
         return blankListPendingOrderData;
     }
 
+    struct BatchPendingLimitOrder {
+        address instance;
+        PendingLimitOrder[] pendingOrders;
+    }
+    function getBatchPendingLimitOrdersByTrader(
+        IMatchingEngineAMM pairManager,
+        address[] memory traders
+    ) public view returns (BatchPendingLimitOrder[] memory batchPendingOrders){
+        batchPendingOrders = new BatchPendingLimitOrder[](traders.length);
+        for (uint i =0; i< traders.length; i++) {
+            batchPendingOrders[i].instance = traders[i];
+            batchPendingOrders[i].pendingOrders = getPendingLimitOrders(pairManager,  traders[i]);
+        }
+    }
+
+    function getBatchPendingLimitOrdersByPair(
+        IMatchingEngineAMM[] memory pairManagers,
+        address trader
+    ) public view returns (BatchPendingLimitOrder[] memory batchPendingOrders){
+        batchPendingOrders = new BatchPendingLimitOrder[](pairManagers.length);
+        for (uint i =0; i< pairManagers.length; i++) {
+            batchPendingOrders[i].instance = address(pairManagers[i]);
+            batchPendingOrders[i].pendingOrders = getPendingLimitOrders(pairManagers[i], trader);
+        }
+    }
+
     function getOrderIdOfTrader(
         address _pairManager,
         address _trader,
